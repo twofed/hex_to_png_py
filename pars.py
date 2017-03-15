0586fefdfa96a6b1b461821c735aa966700d9efd
twofed <whiteodmin@gmail.com>
2017-03-15 11:06:55 +0200

    Add checksum check.

--- pars.py
+++ pars.py
@@ -24,6 +24,12 @@ class hex_to_png():
             nq=ARRAY_HEX[ITER_CNT-1].decode("utf-8")
             asa=nq+nuad
             ARRAY_HEX[ITER_CNT-1]=(hexdata[dr:] + bytes(asa, 'utf-8'))
+        LENS=str(len(ARRAY_HEX))
+        if len(LENS)<7:
+            zero=6-len(LENS)
+            LENS=LENS+'F'*zero
+        ARRAY_HEX.insert(0,bytes(LENS,'utf-8'))
+        #ARRAY_HEX.insert(1,bytes('010101','utf-8'))
         while len(ARRAY_HEX)>ITER_CNT2:
             ARRAY_RGB.append(tuple(int(ARRAY_HEX[ITER_CNT2][i:i+2], 16) for i in (0, 2 ,4)))
             ITER_CNT2 += 1
@@ -47,6 +53,7 @@ class png_to_hex():
         HEX_ARRAY=[]
         INTER_CNT=0
         ITER_CNT2=0
+        outsum = ''
         im = Image.open(filename)
         pix = im.load()
         #print (im.width*im.width)
@@ -57,15 +64,21 @@ class png_to_hex():
             HEX_ARRAY.append(bytes(('%02x%02x%02x' % pix[WIDTH,HIGHT]), 'utf-8'))
             WIDTH+=1
             INTER_CNT+=1
-        search = bytes('000000', 'utf-8')
-        while(len(HEX_ARRAY))!=HEX_ARRAY.index(search)+1:
-            x=(HEX_ARRAY.index(search))
+        chsum=HEX_ARRAY[0].decode("utf-8")
+        INTER_CNT=0
+        while len(chsum)>INTER_CNT:
+            if chsum[INTER_CNT]!='f' and INTER_CNT!=6:
+                outsum=outsum+chsum[INTER_CNT]
+            INTER_CNT += 1
+        HEX_ARRAY.pop(0)
+        while len(HEX_ARRAY)!=int(outsum):
+            x=len(HEX_ARRAY)-1
             HEX_ARRAY.pop(x)
-        HEX_ARRAY.pop(HEX_ARRAY.index(search))
+        #HEX_ARRAY.pop(HEX_ARRAY.index(search))
         with open(('_hexed_'+filename[:-4]), 'wb') as fout:
             while len(HEX_ARRAY)>ITER_CNT2:
                 fout.write(binascii.unhexlify(HEX_ARRAY[ITER_CNT2]))
                 ITER_CNT2 += 1
 
-hex_to_png.main('test.txt')
-png_to_hex.main('test.txt.png')
+hex_to_png.main('test1.7z')
+png_to_hex.main('test1.7z.png')
